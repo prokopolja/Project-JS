507adae05ca7d967c3780cde8a8f9adbf04611fb
prokopolja <kotejechka@gmail.com>
2022-01-29 00:22:46 +0300

    Выполнила втрой урок - использ методы и свойства

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 let title = "JS";
+console.log(typeof title);
+
 let screens = "Простые, Сложные, Интерактивные";
+console.log(screens.length);
+console.log(screens.toLowerCase());
+console.log(screens.split(", "));
+
 let screenPrice = 12;
 let rollback = 78;
 let fullPrice = 2000;
+console.log(typeof fullPrice);
+console.log(fullPrice*(rollback/100));
+console.log("Стоимость верстки экранов " + (screenPrice) + " рублей/долларов/гривен/юаней");
+console.log("Стоимость разработки сайта " + (fullPrice) + " рублей/долларов/гривен/юаней");
+
 let adaptive = true;
-
-console.log("Hello!");
-
-alert("Good luck, friend!");
+console.log(typeof adaptive);
